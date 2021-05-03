f255df5d1c136f28cdab54ee670caf8e296d7497
NicolaGiardino <nicoladgg@protonmail.com>
2021-05-03 16:56:37 +0200

    Update plic.h

--- src/include/devices/plic.h
+++ src/include/devices/plic.h
@@ -48,4 +48,44 @@ int plic_interrupt_enable();
 
 void plic_interrupt_handler();
 
+#include "../../include/platform.h"
+#include <math.h>
+
+int plic_interrupt_enable()
+{
+    uint32_t mie;
+
+    __asm__ volatile("csrr %0, mie" : "=r"(mie));
+
+    if (mie & MIE_MEIE == 0)
+    {
+        mie |= MIE_MEIE;
+        __asm__ volatile("csrw mie, %0" : : "r"(mie));
+        return 0;
+    }
+
+    return -1;
+}
+
+void plic_interrupt_handler()
+{
+
+    uint32_t pending;
+    unsigned int int_number;
+
+    if (pending = PLIC_REG(PLIC_PENDING_OFFSET))
+    {
+        int_number = sqrt(pending) - 1;
+    }
+    else if ((pending = PLIC_REG(PLIC_PENDING_OFFSET_2)) & PLIC_PENDING2_MASK)
+    {
+        int_number = sqrt(pending) + PLIC_ENABLE_OFFSET_MAX;
+    }
+
+    if (irq_functions[int_number].active)
+    {
+        irq_functions[int_number].irq_handler();
+    }
+}
+
 #endif /* PLIC_H */
